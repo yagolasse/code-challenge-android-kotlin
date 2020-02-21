95c31610fd3cd1f2c63ce2d88fffb48052f4568d
Yago Lasse <yago.lasse@cubos.io>
2020-02-21 14:16:14 -0300

    refactor: Removed splash screen and changed to load genres and movies async

--- app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
@@ -7,10 +7,7 @@ import com.arctouch.codechallenge.R
 import com.arctouch.codechallenge.api.TmdbApi
 import com.arctouch.codechallenge.data.Cache
 import kotlinx.android.synthetic.main.home_activity.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import org.koin.android.ext.android.inject
 
 class HomeActivity : AppCompatActivity() {
@@ -22,8 +19,12 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.home_activity)
 
         GlobalScope.launch(Dispatchers.IO) {
-            val upcomingMovies = api.upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, 1, TmdbApi.DEFAULT_REGION)
-            val moviesWithGenres = upcomingMovies.results.map { movie ->
+            val genreResult = async { api.genres(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE) }
+            val upcomingMovies = async {
+                api.upcomingMovies(TmdbApi.API_KEY, TmdbApi.DEFAULT_LANGUAGE, 1, TmdbApi.DEFAULT_REGION)
+            }
+            Cache.cacheGenres(genreResult.await().genres)
+            val moviesWithGenres = upcomingMovies.await().results.map { movie ->
                 movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
             }
             withContext(Dispatchers.Main) {
