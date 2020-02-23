754bcbccc824b3e03e36f771a0f841439366d82d
Yago Lasse <yago.lasse@cubos.io>
2020-02-23 16:31:50 -0300

    feature: Add round corners to movie list and fade edge layout dependency

--- app/src/main/java/com/arctouch/codechallenge/util/GlideExtensions.kt
+++ app/src/main/java/com/arctouch/codechallenge/util/GlideExtensions.kt
@@ -1,6 +1,8 @@
 package com.arctouch.codechallenge.util
 
+import android.content.res.Resources
 import android.graphics.drawable.Drawable
+import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +10,7 @@ import com.arctouch.codechallenge.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestBuilder
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 
 infix fun View.loads(url: String?): RequestBuilder<Drawable> = Glide.with(this).load(url)
@@ -15,16 +18,25 @@ infix fun View.loads(url: String?): RequestBuilder<Drawable> = Glide.with(this).
 infix fun AppCompatActivity.loads(url: String?): RequestBuilder<Drawable> = Glide.with(this).load(url)
 
 infix fun <T> RequestBuilder<T>.asThumbnailInto(imageView: ImageView) {
+    val cornerRadius = 4f.dp(imageView.context.resources)
+
     apply(RequestOptions().placeholder(R.drawable.ic_image_placeholder))
             .thumbnail(0.5f)
             .skipMemoryCache(true)
+            .apply(RequestOptions.bitmapTransform(RoundedCorners(cornerRadius.toInt())))
             .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
             .into(imageView)
 }
 
 infix fun <T> RequestBuilder<T>.on(imageView: ImageView) {
+    val cornerRadius = 8f.dp(imageView.context.resources)
+
     apply(RequestOptions().placeholder(R.drawable.ic_image_placeholder))
             .skipMemoryCache(true)
+            .apply(RequestOptions.bitmapTransform(RoundedCorners(cornerRadius.toInt())))
             .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
             .into(imageView)
 }
+
+fun Float.dp(resources: Resources): Float =
+        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, resources.displayMetrics)
